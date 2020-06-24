d9b89a522fdc45e81589a9a9124f9c0e54926bd4
Dahye Min <dhmin401@gmail.com>
2020-06-23 23:33:52 -0400

    edit script.js

--- script.js
+++ script.js
@@ -2,28 +2,32 @@ const proxy = "https://cors-anywhere.herokuapp.com/";
 var xmlhttp = new XMLHttpRequest();
 var latitude;
 var longitude;
+var position;
+var map;
 
 xmlhttp.onreadystatechange = function() {
   if(this.readyState == 4 && this.status == 200) {
     var scheduleObj = JSON.parse(this.responseText);
     latitude = scheduleObj.iss_position.latitude;
     longitude = scheduleObj.iss_position.longitude;
-
-    var map;
-    var position = { lat: parseFloat(latitude), lng: parseFloat(longitude) };
-    function initMap() {
-      map = new google.maps.Map(document.getElementById("map"), {
-        center: position,
-        zoom: 2,
-        mapTypeId: 'satellite'
-      });
-      var marker = new google.maps.Marker({
-        position: position,
-        map: map
-      })
-    }
+    position = { lat: parseFloat(latitude), lng: parseFloat(longitude) };
+    initMap(); 
   }
 };
+
+function initMap() {
+  if(latitude !== null && longitude !== null) {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: position,
+      zoom: 2,
+      mapTypeId: 'satellite'
+    });
+    var marker = new google.maps.Marker({
+      position: position,
+      map: map
+    })
+  }
+}
 
 xmlhttp.open("GET", `${proxy}http://api.open-notify.org/iss-now.json`, true);
 xmlhttp.send();
