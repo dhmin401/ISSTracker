7daa4cc8784bf7b8b92ae7fc92b17f5a72157b54
Dahye Min <dhmin401@gmail.com>
2020-06-23 21:56:05 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var xmlhttp = new XMLHttpRequest();
+var latitude;
+var longitude;
+
+xmlhttp.onreadystatechange = function() {
+  if(this.readyState == 4 && this.status == 200) {
+    var scheduleObj = JSON.parse(this.responseText);
+    latitude = scheduleObj.iss_position.latitude;
+    longitude = scheduleObj.iss_position.longitude;
+
+    var map;
+    var position = { lat: parseFloat(latitude), lng: parseFloat(longitude) };
+
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: position,
+      zoom: 2,
+      mapTypeId: 'satellite'
+    });
+    var marker = new google.maps.Marker({
+      position: position,
+      map: map
+    })
+  }
+};
+
+xmlhttp.open("GET", "http://api.open-notify.org/iss-now.json", true);
+xmlhttp.send();
