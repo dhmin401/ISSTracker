4150c6a024cb8785eb0d7a0be9c6ed16a9b478b7
Dahye Min <48623630+dhmin401@users.noreply.github.com>
2020-11-22 20:23:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -52,10 +52,10 @@ var x = setInterval(function() {
   var now = new Date().getTime();
   var distance = countDownDate - now;
 
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24)) * -1;
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) * -1;
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)) * -1;
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000) * -1;
 
   document.getElementById("timer").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
